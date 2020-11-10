e85322b021ca3c0c52bec3a186672faf406aca07
andriimy <Andrii.Myrhorodskyi@nice.com>
2020-11-10 15:42:51 +0200

    Fixed checkout args

--- src/com/ghotfall/spl/steps/Common.groovy
+++ src/com/ghotfall/spl/steps/Common.groovy
@@ -11,7 +11,14 @@ def getRepo() {
     stage('Checkout') {
         echo 'Checkout:'
 //        checkout scm
-        checkout([$class: 'GitSCM', extensions: [[$class: 'LocalBranch', localBranch: "**"]]])
+//        checkout([$class: 'GitSCM', extensions: [[$class: 'LocalBranch', localBranch: "**"]]])
+        checkout([
+                $class: 'GitSCM',
+                branches: scm.branches,
+                doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
+                extensions: scm.extensions + [[$class: 'LocalBranch', localBranch: "**"]],
+                userRemoteConfigs: scm.userRemoteConfigs
+        ])
     }
 }
 
