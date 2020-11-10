83d30fde030ad97d408070fe9d7245b444a5fd10
andriimy <Andrii.Myrhorodskyi@nice.com>
2020-11-10 15:21:00 +0200

    Changed updateVersion method #2

--- src/com/ghotfall/spl/steps/Maven.groovy
+++ src/com/ghotfall/spl/steps/Maven.groovy
@@ -25,22 +25,29 @@ def updateVersion() {
 
         def pom = readMavenPom file: 'pom.xml'
         echo "Current version is ${pom.getVersion()}"
-//        try {
             def (major, minor, patch) = pom.getVersion().tokenize('.')
-            minor++
-            pom.setVersion("${major}.${minor}.${patch}")
-            writeMavenPom model: pom
-            echo "New version is ${pom.getVersion()}"
-
-            echo 'Writing changes to repository'
-            git status
-            git add 'pom.xml'
-            git commit -m "Jenkins CI - Project version increment"
-            git push origin
-//        } catch(Exception ignored) {
-//            echo 'Unknown version format, skipping version increment'
-//        }
+            if (minor != null) {
+                minor++
+                String newVersion = "${major}.${minor}.${patch}"
+                pom.setVersion(newVersion)
+                writeMavenPom model: pom
+                echo "New version is $newVersion"
+                writePOMToRepo(newVersion)
+            } else {
+                echo 'Unknown version format, skipping'
+            }
     }
+}
+
+private def writePOMToRepo(String version) {
+    echo 'Writing changes to repository'
+
+    bat """\
+git status
+git add pom.xml
+git commit -m "Jenkins CI - New version: ${version}"
+git push origin
+"""
 }
 
 return this
